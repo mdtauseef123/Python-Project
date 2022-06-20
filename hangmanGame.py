4782a667e8c2a15a6a199c72930b8dff417b4fcd
Md Tauseef Akhtar <63928008+mdtauseef123@users.noreply.github.com>
2022-06-20 16:06:09 +0530

    Update hangmanGame.py

--- hangmanGame.py
+++ hangmanGame.py
@@ -9,7 +9,7 @@ stages=hangman_art.stages
 arr = hangman_words.word_list
 index=random.randint(0,len(arr)-1)
 chosen_word = arr[index]
-
+print(f"Word is {chosen_word}")
 display=[]
 for i in range(0,len(chosen_word)):
 	display.append("_")
@@ -17,8 +17,15 @@ print("Word Format:-")
 print(''.join(display))
 ctr=0
 lives=6
+prev=""
 while ctr!=len(chosen_word) and lives!=-1:
 	guess=input("Guess a letter: ").lower()
+	#If the correct guess is repeated then only we will throw the message and doesn't count it
+	if prev==guess:
+		print(f"You have already guessed {guess}")
+		continue
+	else:
+		prev=guess
 	if not guess in chosen_word:
 		print("Wrong Guess")
 		print(stages[lives])

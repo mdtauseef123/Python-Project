a7bbe1bb1cceb5554ac06dabdd111c84142194da
Md Tauseef Akhtar <63928008+mdtauseef123@users.noreply.github.com>
2022-06-23 19:14:55 +0530

    Update HigherLowerGame.py

--- HigherLowerGame.py
+++ HigherLowerGame.py
@@ -11,6 +11,11 @@ score=0
 while gameContinue:
 	secondDataIndex=random.randint(0,len(datas)-1)
 	secondData=datas[secondDataIndex]
+	#In case if both the same account is being fetched randomly
+	#We will keep generating the random account until we get different account
+	while firstData==secondData:
+		secondDataIndex=random.randint(0,len(datas)-1)
+		secondData=datas[secondDataIndex]
 	print(f"Compare A: {firstData['name']}, a {firstData['description']}, from {firstData['country']}.")
 	print(art.vs)
 	print(f"Against B: {secondData['name']}, a {secondData['description']}, from {secondData['country']}.")

1eecd2ccf0752bc6b6102d8e69e643c23f4f7762
Md Tauseef Akhtar <63928008+mdtauseef123@users.noreply.github.com>
2022-06-23 19:07:12 +0530

    Create HigherLowerGame.py

---
+++ HigherLowerGame.py
@@ -0,0 +1,31 @@
+import art
+import random
+import game_data
+import os
+print(art.logo)
+datas=game_data.data
+firstDataIndex=random.randint(0,len(datas)-1)
+firstData=datas[firstDataIndex]
+gameContinue=True
+score=0
+while gameContinue:
+	secondDataIndex=random.randint(0,len(datas)-1)
+	secondData=datas[secondDataIndex]
+	print(f"Compare A: {firstData['name']}, a {firstData['description']}, from {firstData['country']}.")
+	print(art.vs)
+	print(f"Against B: {secondData['name']}, a {secondData['description']}, from {secondData['country']}.")
+	choice=input("Who has more followers? Type 'A' or 'B': ")
+	if firstData['follower_count']>secondData['follower_count']:
+		ans="A"
+	else:
+		ans="B"
+	if choice==ans:
+		score+=1
+		gameContinue=True
+		os.system('clear')
+		print(f"You\'re right! Current score: {score}.")
+		firstData=secondData
+	else:
+		gameContinue=False
+		os.system('clear')
+		print(f"Sorry, that\'s wrong. Final score: {score}")
